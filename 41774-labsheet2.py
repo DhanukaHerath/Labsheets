d8526ef17dc52f3969ae85eb234f32896ae11d22
Dhanuka1231 <dhanukaherath1231@gmail.com>
2026-05-11 11:45:14 +0530

    Create 41774-labsheet2.py

---
+++ 41774-labsheet2.py
@@ -0,0 +1,13 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Dhanuka Herath")
+Dhanuka Herath
+>>> print("Kingswood College")
+Kingswood College
+>>> #single line comment
+>>> """Double line comment
+... double line comment"""
+'Double line comment\ndouble line comment'
+>>> # we use comments to document things such as code explainatons, document functionalty without  affecting program execution
+>>> 22/7
+3.142857142857143
